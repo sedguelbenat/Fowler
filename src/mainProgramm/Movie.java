024ca7da13c7eb9cd5dc70b0d45a3cbe92b93ca5
Sedar Gülbenat <sedar_g_1997@web.de>
2018-05-22 21:59:25 +0200

    STEP: Moved amountFor() from Rental to Movie

--- src/mainProgramm/Movie.java
+++ src/mainProgramm/Movie.java
@@ -18,4 +18,24 @@ public class Movie {
     public String getTitle (){
         return title;
     };
+    public double amountFor(int daysRented) {
+        double thisAmount = 0;
+        switch (this.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
+    }
+
 }
